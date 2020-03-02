920689860731093d9bf9cba235baf9fc48052ce3
SabGalaxy <sabarishraj88@hotmail.com>
2020-03-01 21:31:13 -0500

    Added Tuscan Production api token
    Currently has the production endpoint and access token

--- Tuscan/constants.js
+++ Tuscan/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
-    requestUrl: 'https://stage.tuscanyleather.it/api/v1/',
+    requestUrl: 'https://www.tuscanyleather.it/api/v1/',
     authorization: {
-        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjE2NjY2OCwiaXNzIjoiaHR0cHM6Ly93d3cudHVzY2FueWxlYXRoZXIuaXQvcmVmcmVzaC1hcGktdG9rZW4iLCJpYXQiOjE1ODE5MDIzNTMsImV4cCI6MTg5NzI2MjM1MywibmJmIjoxNTgxOTAyMzUzLCJqdGkiOiJ2aW5LdlpIUmZJUTZlc3BhIn0.FP9_TgqsunJBNSXkIz4zRWYAIRKjtS11ptGrSCy4v3Y'
+        'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjE2NjY2OCwiaXNzIjoiaHR0cHM6Ly93d3cudHVzY2FueWxlYXRoZXIuaXQvcmVmcmVzaC1hcGktdG9rZW4iLCJpYXQiOjE1ODMxMTU0NTAsImV4cCI6MTg5ODQ3NTQ1MCwibmJmIjoxNTgzMTE1NDUwLCJqdGkiOiJUUDlHUE1NUUhPUDNoM2JXIn0.fAurowYbeHAWUvWRTeazFOMgZc4NOdlS9yexIkcgrmg'
     }
   };
